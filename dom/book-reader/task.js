dbafa15c2ba383c4c4f2886e5d3d649e838794db
Ira Vakhnina <pirania943@gmail.com>
2020-09-22 18:40:30 +0300

    first commit

---
+++ dom/book-reader/task.js
@@ -0,0 +1,20 @@
+const fontSize = Array.from(document. querySelectorAll('.font-size'));
+
+fontSize.forEach((elem) => {
+	elem.addEventListener('click', function(e){
+		e.preventDefault();
+		document.querySelector('.font-size_active').classList.remove('font-size_active');
+		
+		this.classList.add('font-size_active');
+		this.closest('.book').classList.remove("book_fs-big");
+		this.closest('.book').classList.remove("book_fs-small");
+
+		if(elem.classList.contains("font-size_small")){
+			this.closest('.book').classList.add("book_fs-small");
+		}
+
+		if(elem.classList.contains("font-size_big")){
+			this.closest('.book').classList.add("book_fs-big");
+		}
+	})
+});
