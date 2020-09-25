0688e465f905223b656099e88aa83ae3cc938b89
Ira Vakhnina <pirania943@gmail.com>
2020-09-25 20:55:53 +0300

    firsr commit

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,9 @@
+const tooltip = Array.from(document.querySelectorAll('.has-tooltip'));
+tooltip.forEach(link => { 
+
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        link.insertAdjacentHTML('afterend', `<div class="tooltip">${link.getAttribute('title')}</div>`);
+        document.querySelector('.tooltip').classList.toggle('tooltip_active');
+    })
+});
