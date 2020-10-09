0bae3f911bad9ffbf1ef4d5937b35346e5cd2514
Ira Vakhnina <pirania943@gmail.com>
2020-10-09 13:20:07 +0300

    first

---
+++ client-state/popup/task.js
@@ -0,0 +1,11 @@
+const modal = document.getElementById('subscribe-modal');
+if (!document.cookie.includes('modal=closed')) {
+  modal.classList.add('modal_active');
+}
+
+document.addEventListener('click', click => {
+  if (click.target.classList.contains('modal__close')) {
+    modal.classList.remove('modal_active');
+    document.cookie = 'modal=closed';
+  }
+})
