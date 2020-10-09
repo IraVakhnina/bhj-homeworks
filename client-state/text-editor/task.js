6f963c7a0ba388f9bd93604cf0f48e51ddb50846
Ira Vakhnina <pirania943@gmail.com>
2020-10-09 11:19:28 +0300

    first

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,6 @@
+const textarea = document.getElementById('editor');
+textarea.addEventListener('input', event => {
+    localStorage.setItem('text', textarea.value);
+})
+
+textarea.value = localStorage.getItem('text');
